27d961d516d9359f5bfe40a15db2076c8258d28e
Mishimwe <honoreishimwe6@gmail.com>
2024-04-21 19:22:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def entrypoint():
     return 'Hello World!'
 
-if name == '_main':
+if name == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
